11545e9e1ea1c81b7b9833da941ddf3eb46bf781
wagyu298 <1514790+wagyu298@users.noreply.github.com>
2023-02-08 20:39:55 +0900

    Update owner verification addresses

--- packages/nouns-contracts/tasks/cnnouns-verify-ownership.ts
+++ packages/nouns-contracts/tasks/cnnouns-verify-ownership.ts
@@ -6,37 +6,37 @@ task('cnnouns-verify-ownership', 'Verify ownership of contracts')
   .addOptionalParam(
     'nounsDaoExecutor',
     'The `NounsDAOExecutor` contract address',
-    '0xDa06AC4272B4D050e1b64B35d66FAa5E2D3092ec',
+    '0x5FBf55eEc421db4bdEd1E2098048384fa18dC0B3',
     types.string,
   )
   .addOptionalParam(
     'nounsDescriptorV2',
     'The `NounsDescriptorV2` contract address',
-    '0x0FC517F1f5f632b459587fa432083Ef9564c7E3C',
+    '0x00Ad9422fF4A060858f184Afdf7576111076ccDF',
     types.string,
   )
   .addOptionalParam(
     'nftDescriptorV2',
     'The `NFTDescriptorV2` contract address',
-    '0x8428537376334A145874158477ec1c48CFe9F169',
+    '0xf4aE9A4559523bf2f850d9eBC7f84544aBda1735',
     types.string,
   )
   .addOptionalParam(
     'nounsToken',
     'The `NounsToken` contract address',
-    '0xc16521E252A44424eD0e50da1167e8a9c8a3c8a2',
+    '0xa0C8D694baCeeC69532E1547514B50006381dAAF',
     types.string,
   )
   .addOptionalParam(
     'nounsAuctionHouseProxyAdmin',
     'The `NounsAuctionHouseProxyAdmin` contract address',
-    '0xb212e37d7FE3ba41143418756Cb914ae1177A602',
+    '0xAADe9aB82135F341EA175d2414e49D6A8b36B28F',
     types.string,
   )
   .addOptionalParam(
     'nounsAuctionHouseProxy',
     'The `NounsAuctionHouseProxy` contract address',
-    '0x765E17E1F4b4b4d2e6f42413dDE3B7CfEa8bB1fC',
+    '0x99A8AFdb4f8D6c02Cb5D5C27769a056aC79e45Fd',
     types.string,
   )
   .setAction(async (args, { ethers, run }) => {
