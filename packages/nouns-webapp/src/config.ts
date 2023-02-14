9cad5ff6894d9e957516761ff0401c48069cd1eb
wagyu298 <1514790+wagyu298@users.noreply.github.com>
2023-02-14 17:27:01 +0900

    Undefine external contracts address (w/ comment-outed)

--- packages/nouns-webapp/src/config.ts
+++ packages/nouns-webapp/src/config.ts
@@ -88,23 +88,23 @@ const app: Record<SupportedChains, AppConfig> = {
 
 const externalAddresses: Record<SupportedChains, ExternalContractAddresses> = {
   [ChainId.Rinkeby]: {
-    lidoToken: '0xF4242f9d78DB7218Ad72Ee3aE14469DBDE8731eD',
-    usdcToken: '0xeb8f08a975Ab53E34D8a0330E0D34de942C95926',
+    lidoToken: undefined, // '0xF4242f9d78DB7218Ad72Ee3aE14469DBDE8731eD',
+    usdcToken: undefined, // '0xeb8f08a975Ab53E34D8a0330E0D34de942C95926',
     payerContract: undefined,
     tokenBuyer: undefined,
     chainlinkEthUsdc: undefined,
   },
   [ChainId.Goerli]: {
-    lidoToken: '0x2DD6530F136D2B56330792D46aF959D9EA62E276',
-    usdcToken: '0x07865c6e87b9f70255377e024ace6630c1eaa37f',
+    lidoToken: undefined, // '0x2DD6530F136D2B56330792D46aF959D9EA62E276',
+    usdcToken: undefined, // '0x07865c6e87b9f70255377e024ace6630c1eaa37f',
     payerContract: undefined,
     tokenBuyer: undefined,
     chainlinkEthUsdc: undefined,
   },
   [ChainId.Mainnet]: {
-    lidoToken: '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
-    usdcToken: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-    chainlinkEthUsdc: '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
+    lidoToken: undefined, // '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
+    usdcToken: undefined, // '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    chainlinkEthUsdc: undefined, // '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
     payerContract: undefined,
     tokenBuyer: undefined,
   },
