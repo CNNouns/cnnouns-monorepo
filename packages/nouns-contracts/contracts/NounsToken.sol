585cf58d38660db32fd4f0c82a639a36e4a89905
wagyu298 <1514790+wagyu298@users.noreply.github.com>
2023-02-15 11:40:19 +0900

    chore: comments

--- packages/nouns-contracts/contracts/NounsToken.sol
+++ packages/nouns-contracts/contracts/NounsToken.sol
@@ -150,7 +150,8 @@ contract NounsToken is INounsToken, Ownable, ERC721Checkpointable {
     /**
      * @notice Mint a Noun to the minter, along with a possible nounders reward
      * Noun. Nounders reward Nouns are minted every 10 Nouns, starting at 0,
-     * until 183 nounder Nouns have been minted (5 years w/ 24 hour auctions).
+     * until 183 nounder Nouns have been minted (5 years w/ 24 hour plus growth
+     * auctions).
      * @dev Call _mintTo with the to address(es).
      */
     function mint() public override onlyMinter returns (uint256) {
